3d02bbf4ccfd956785b0be6220bcff68aab12ea5
SolarEarth37 <sebastianbusk@live.dk>
2025-11-12 11:08:41 +0100

    Update .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,8 +1,22 @@
 module.exports = {
-  extends: ["next/core-web-vitals"],
+  root: true,
   rules: {
     "react/react-in-jsx-scope": "off",
     "react/jsx-uses-react": "off",
     "no-import-assign": "off",
   },
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "react"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "next/core-web-vitals"
+  ],
+  overrides: [
+    {
+      files: ["*.ts","*.tsx"],
+      parserOptions: { project: "./tsconfig.json", ecmaFeatures: { jsx: true } },
+    }
+  ]
 };
